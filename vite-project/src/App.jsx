f060adf2047bbb910c555db1d12e7a15bd125d33
Kehinde Malagu <Kehindemalagu@gmail.com>
2023-02-01 13:40:52 +0100

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1 className="text-5xl">HELLO!</h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/App.jsx' , './index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
